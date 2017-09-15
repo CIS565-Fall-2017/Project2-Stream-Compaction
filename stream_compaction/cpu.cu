eb640bd9cabc38d0b4c6a026e07abf8e3c1b6c85
rms13 <rishabhs@seas.upenn.edu>
2017-09-15 00:31:20 -0400

    part 1

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -17,9 +17,18 @@ namespace StreamCompaction {
          * For performance analysis, this is supposed to be a simple for loop.
          * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function first.
          */
+        void scan_implementation(int n, int *odata, const int *idata) {
+          // your actual implementation
+          odata[0] = 0;
+          for (int i = 1; i < n; i++) {
+            odata[i] = odata[i - 1] + idata[i - 1];
+          }
+        }
+
         void scan(int n, int *odata, const int *idata) {
 	        timer().startCpuTimer();
             // TODO
+          scan_implementation(n, odata, idata);
 	        timer().endCpuTimer();
         }
 
@@ -31,8 +40,15 @@ namespace StreamCompaction {
         int compactWithoutScan(int n, int *odata, const int *idata) {
 	        timer().startCpuTimer();
             // TODO
+          int next = 0;
+          for (int i = 0; i < n; i++) {
+            if (idata[i] != 0) {
+              odata[next] = idata[i];
+              next++;
+            }
+          }
 	        timer().endCpuTimer();
-            return -1;
+          return next;
         }
 
         /**
@@ -43,8 +59,32 @@ namespace StreamCompaction {
         int compactWithScan(int n, int *odata, const int *idata) {
 	        timer().startCpuTimer();
 	        // TODO
+          int *mapped = new int[n];
+          int *scanned = new int[n];
+          int count = 0;
+          // Map
+          for (int i = 0; i < n; i++) {
+            if (idata[i] != 0) {
+              mapped[i] = 1;
+            }
+            else {
+              mapped[i] = 0;
+            }
+          }
+
+          // Scan
+          scan_implementation(n, scanned, mapped);
+
+          // Scatter
+          for (int i = 0; i < n; i++) {
+            if (mapped[i] == 1) {
+              odata[scanned[i]] = idata[i];
+              count++;
+            }
+          }
+
 	        timer().endCpuTimer();
-            return -1;
+          return count;
         }
     }
 }
