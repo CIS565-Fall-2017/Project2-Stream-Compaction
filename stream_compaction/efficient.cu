20a68c51ed99ba8a5f9ff7c2b1a4872712afaadf
Jon Lee <agentlee927@gmail.com>
2017-09-15 18:42:19 -0400

    debugging efficient

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "efficient.h"
 
+#define blocksize 128
+
 namespace StreamCompaction {
     namespace Efficient {
         using StreamCompaction::Common::PerformanceTimer;
@@ -12,12 +14,71 @@ namespace StreamCompaction {
             return timer;
         }
 
+		__global__ void upSweep(int n, int pow2dPlus1, int pow2d, int *odata)
+		{
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= n) {
+				return;
+			}
+
+			index *= pow2dPlus1;
+			if(index < n)
+				odata[index + pow2dPlus1 - 1] += odata[index + pow2d - 1];
+		}
+
+		__global__ void downSweep(int n, int pow2dPlus1, int pow2d, int *odata)
+		{
+			int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= n) {
+				return;
+			}
+
+			index *= pow2dPlus1;
+			if (index < n) {
+				int t = odata[index + pow2d - 1];
+				odata[index + pow2d - 1] = odata[index + pow2dPlus1 - 1];
+				odata[index + pow2dPlus1 - 1] += t;
+			}
+		}
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
+			dim3 fullBlocksPerGrid((n + blocksize - 1) / blocksize);
+			
+			int *out;
+			cudaMalloc((void**)&out, n * sizeof(int));
+			cudaMemcpy(out, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
+			timer().startGpuTimer();
             // TODO
+
+			// Get the next power of 2
+			int currPow = ilog2ceil(n) - 1;
+			int nextPow = 2 << currPow;
+
+			// Up-Sweep
+			for (int d = 0; d <= ilog2ceil(n) - 1; d++) {
+				int pow2dPlus1 = pow(2, d + 1);
+				int pow2d = pow(2, d);
+
+				upSweep << < fullBlocksPerGrid, blocksize >> > (n, pow2dPlus1, pow2d, out);
+			}
+
+			// Down-Sweep
+			odata[n - 1] = 0;
+			for (int d = ilog2ceil(n) - 1; d >= 0; d--) {
+				int pow2dPlus1 = pow(2, d + 1);
+				int pow2d = pow(2, d);
+			
+				downSweep << < fullBlocksPerGrid, blocksize >> > (n, pow2dPlus1, pow2d, out);
+			}
+
+			cudaMemcpy(odata, out, sizeof(int) * n, cudaMemcpyDeviceToHost);
+
+			cudaFree(out);
+
             timer().endGpuTimer();
         }
 
