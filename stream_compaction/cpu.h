e4944ab55877847cf9077a8049acd12c6581c8f8
Rudraksha20 <rdimple@seas.upenn.edu>
2017-09-16 10:43:55 -0400

    CPU Implementation Complete

--- stream_compaction/cpu.h
+++ stream_compaction/cpu.h
@@ -8,8 +8,14 @@ namespace StreamCompaction {
 
         void scan(int n, int *odata, const int *idata);
 
+		void scanExclusivePrefixSum(int n, int *odata, const int *idata);
+
         int compactWithoutScan(int n, int *odata, const int *idata);
 
         int compactWithScan(int n, int *odata, const int *idata);
+
+		int scatter(int n, int *mapped, int *scanned, const int *idata, int *odata);
+
+		void map(int n, int *mapped, const int *idata);
     }
 }
