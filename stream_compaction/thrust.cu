4e904fac2c5da4993997c116f1987214350cdeed
RICKY RAJANI <rajanir@dc1.base.seas.upenn.edu>
2017-09-19 12:58:49 -0400

    fixed images and thrust implementation

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -18,14 +18,37 @@ namespace StreamCompaction {
       * Performs prefix-sum (aka scan) on idata, storing the result into odata.
       */
     void scan(int n, int *odata, const int *idata) {
-      timer().startGpuTimer();
       // TODO use `thrust::exclusive_scan`
       // example: for device_vectors dv_in and dv_out:
       // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
 
-      thrust::exclusive_scan(idata, idata + n, odata);
+      int *idataSwap;
+      int *odataSwap;
+
+      cudaMalloc((void**)&idataSwap, n * sizeof(int));
+      checkCUDAError("cudaMalloc for idataSwap failed!");
+
+      cudaMalloc((void**)&odataSwap, n * sizeof(int));
+      checkCUDAError("cudaMalloc for odataSwap failed!");
+
+      // Copy from CPU to GPU
+      cudaMemcpy(idataSwap, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+      checkCUDAError("cudaMemcpy failed!");
+
+      timer().startGpuTimer();
+
+      thrust::device_ptr<int> dev_thrust_idata(idataSwap);
+      thrust::device_ptr<int> dev_thrust_odata(odataSwap);
+
+      thrust::exclusive_scan(dev_thrust_idata, dev_thrust_idata + n, dev_thrust_odata);
 
       timer().endGpuTimer();
+
+      // Copy from GPU back to CPU
+      cudaMemcpy(odata, odataSwap, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+      cudaFree(idataSwap);
+      cudaFree(odataSwap);
     }
   }
 }
