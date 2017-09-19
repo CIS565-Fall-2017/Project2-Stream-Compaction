16dbe99329b6231d57a02988f7701aacbb3465cd
sarahforcier <sforcier@seas.upenn.edu>
2017-09-19 17:54:20 -0400

    questions

--- README.md
+++ README.md
@@ -6,10 +6,15 @@ CUDA Stream Compaction
 Sarah Forcier
 Tested on GeForce GTX 1070
 
-### Algorithm Descriptions
+### Parallel Algorithm Descriptions
 #### Scan
+Computes a exclusive sum of all integers that came before each element.
+
 #### Stream Compaction
+Removes 0's from an array of integers. 
+
 #### Radix Sort
+Sorts an array of integers from low to high.
 
 ### Test Program Output
 
@@ -98,28 +103,32 @@ Tested on GeForce GTX 1070
 
 ### Performance Analysis
 
-#### Scan
-
 | Scan for SIZE < 20K | Scan for SIZE > 4M | 
 | ------------- | ----------- |
 | ![](img/scan20K.png) | ![](img/scan4m.png) |
 
-![](img/scan20k.png) ![](img/scan4K.png)
-
-#### Stream Compaction
+The CPU method on non-power-of-two arrays is faster than that for power-of-two arrays because the CPU performs serially, and the non-power-of-two arrays have 3 fewer elements, so it finished faster because it has 3 less iterations in for-loops. The difference in performance between these differently sized arrays on the GPU side is the opposite because warps are allocated for 32 threads, so non-power-of-two sized arrays cannot fill an entire warp. Thus not all threads are utilized and the warp has lower occupancy. For an array with 3 less elements than a power-of-two (as was tested here), the different is marginal. But the difference is much more apparent for arrays with 1 more element than a power-of-two. 
 
 | Compact for SIZE < 20K | Compact for SIZE > 4M | 
 | ------------- | ----------- |
 | ![](img/compact20k.png) | ![](img/compact4m.png) |
 
-#### Radix Sort
+The difference in performance between the CPU method, the CPU method with scan, and the GPU efficient method illustrates why different algorithms are required for parallel and serial executes. The CPU method with scan is a CPU version of the GPU efficient method. Without multiple threads running at once, the CPU version must rely on multiple for-loops, more than its compaction implementation without scan. Therefore it is the slowest of the methods.
 
 | Radix Sort for SIZE < 20K | Radix Sort for SIZE > 4M | 
 | ------------- | ----------- |
 | ![](img/radix20k.png) | ![](img/radix4m.png) |
 
+Much like scan and compaction, GPU radix sort performs faster for large array sizes even though parallel radix sort requires more sort passes becauce the GPU works on bits in order to benefit from scan whereas the CPU version works on digits in decimal. The CPU algorithm calculates the maximum value in the array to determine how many sort passes are required. The maximum number of passes the GPU could do on an integer array is 32, but since the CPU version only loops for the maximum number of digits. So for performance comparison sake, the GPU method takes the number of bits in the input array that contain 1s, and the calculation of the CPU maximum is not included in the time.  
+
 ### Q&A
 
 #### Compare GPU Scan implementations to the serial CPU version?
+The GPU version performs better for scan, stream compaction, and radix sort for large arrays (SIZE < 2^[18]). Since the intersection between the CPU and GPU methods occurs at the same sized array, it suggests that the slow GPU performance for small arrays is based on a overhead required for creating and distributing threads. One that is only surpassed with large arrays.  
 
 #### Where are the performance bottlenecks?
+For the work efficient implementation, the upsweep and downsweep kernel functions require a similar amount of time, because they have the same ratio of compute to memory instructions. Most time is spent in memory reads and writes, but since there are not many of either type of instruction, it is not possible to space out the memory instructions with computations. 
+
+#### How does the GPU Scan implementation compare to thrust?
+Thrust exclusive scan on power-of-two sized arrays performs poorly but consistently for most array size except the largest. The consistency indicates that the thrust implementation might have a large overhead. While other implementations' runtimes increase exponentially with array size, thrust with power-of-two arrays remains around 10ms for all array sizes. However, the thrust method on non-power-of-two arrays behaves more closely to the other gpu scan implementations with an exponential increase but with a much better performance. An explanation for the poor performance with power-of-two arrays might be that thrust implementation might allocate and compute on more memory with these sizes but not for non-power-of-two arrays - the opposite of what the work efficient method does. According to the Nsight performance analysis, thrust also has low warp occupancy (25%) and high number of required registers per thread (78), which could also explain the poor perform for these sized arrays. However, thrust makes good use of shared memory.
+
--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #include <stream_compaction/thrust.h>
 #include <stream_compaction/radix.h>
 #include "testing_helpers.hpp"
-const int SIZE = 1 << 4; // feel free to change the size of array
+const int SIZE = 1 << 10; // feel free to change the size of array
 const int NPOT = SIZE - 3; // Non-Power-Of-Two
 //int a[SIZE], b[SIZE], c[SIZE], d[SIZE];
 
@@ -100,87 +100,87 @@ int main(int argc, char* argv[]) {
 
     // Compaction tests
 
-    genArray(SIZE - 1, a, 4);  // Leave a 0 at the end to test that edge case
-    a[SIZE - 1] = 0;
-    printArray(SIZE, a, true);
+ //   genArray(SIZE - 1, a, 4);  // Leave a 0 at the end to test that edge case
+ //   a[SIZE - 1] = 0;
+ //   printArray(SIZE, a, true);
 
-    int countSIZE, countNPOT, expectedSIZE, expectedNPOT;
+ //   int countSIZE, countNPOT, expectedSIZE, expectedNPOT;
 
-    // initialize b using StreamCompaction::CPU::compactWithoutScan you implement
-    // We use b for further comparison. Make sure your StreamCompaction::CPU::compactWithoutScan is correct.
-    zeroArray(SIZE, b);
-    printDesc("cpu compact without scan, power-of-two");
-    countSIZE = StreamCompaction::CPU::compactWithoutScan(SIZE, b, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
-	expectedSIZE = countSIZE;
-    printArray(expectedSIZE, b, true);
-    printCmpLenResult(countSIZE, expectedSIZE, b, b);
+ //   // initialize b using StreamCompaction::CPU::compactWithoutScan you implement
+ //   // We use b for further comparison. Make sure your StreamCompaction::CPU::compactWithoutScan is correct.
+ //   zeroArray(SIZE, b);
+ //   printDesc("cpu compact without scan, power-of-two");
+ //   countSIZE = StreamCompaction::CPU::compactWithoutScan(SIZE, b, a);
+ //   printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+	//expectedSIZE = countSIZE;
+ //   printArray(expectedSIZE, b, true);
+ //   printCmpLenResult(countSIZE, expectedSIZE, b, b);
 
-    zeroArray(SIZE, c);
-    printDesc("cpu compact without scan, non-power-of-two");
-	countNPOT = StreamCompaction::CPU::compactWithoutScan(NPOT, c, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
-    expectedNPOT = countNPOT;
-    printArray(countNPOT, c, true);
-    printCmpLenResult(countNPOT, expectedNPOT, b, c);
+ //   zeroArray(SIZE, c);
+ //   printDesc("cpu compact without scan, non-power-of-two");
+	//countNPOT = StreamCompaction::CPU::compactWithoutScan(NPOT, c, a);
+ //   printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+ //   expectedNPOT = countNPOT;
+ //   printArray(countNPOT, c, true);
+ //   printCmpLenResult(countNPOT, expectedNPOT, b, c);
 
-    zeroArray(SIZE, c);
-    printDesc("cpu compact with scan");
-	countSIZE = StreamCompaction::CPU::compactWithScan(SIZE, c, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
-    printArray(countSIZE, c, true);
-    printCmpLenResult(countSIZE, expectedSIZE, b, c);
+ //   zeroArray(SIZE, c);
+ //   printDesc("cpu compact with scan");
+	//countSIZE = StreamCompaction::CPU::compactWithScan(SIZE, c, a);
+ //   printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+ //   printArray(countSIZE, c, true);
+ //   printCmpLenResult(countSIZE, expectedSIZE, b, c);
 
-    zeroArray(SIZE, c);
-    printDesc("work-efficient compact, power-of-two");
-	countSIZE = StreamCompaction::Efficient::compact(SIZE, c, a);
-    printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    printArray(expectedSIZE, c, true);
-    printCmpLenResult(countSIZE, expectedSIZE, b, c);
+ //   zeroArray(SIZE, c);
+ //   printDesc("work-efficient compact, power-of-two");
+	//countSIZE = StreamCompaction::Efficient::compact(SIZE, c, a);
+ //   printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
+ //   printArray(expectedSIZE, c, true);
+ //   printCmpLenResult(countSIZE, expectedSIZE, b, c);
 
-    zeroArray(SIZE, c);
-    printDesc("work-efficient compact, non-power-of-two");
-	countNPOT = StreamCompaction::Efficient::compact(NPOT, c, a);
-    printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    printArray(expectedNPOT, c, true);
-    printCmpLenResult(countNPOT, expectedNPOT, b, c);
+ //   zeroArray(SIZE, c);
+ //   printDesc("work-efficient compact, non-power-of-two");
+	//countNPOT = StreamCompaction::Efficient::compact(NPOT, c, a);
+ //   printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
+ //   printArray(expectedNPOT, c, true);
+ //   printCmpLenResult(countNPOT, expectedNPOT, b, c);
 
-    printf("\n");
-    printf("*****************************\n");
-    printf("** RADIX SORT TESTS **\n");
-    printf("*****************************\n");
+ //   printf("\n");
+ //   printf("*****************************\n");
+ //   printf("** RADIX SORT TESTS **\n");
+ //   printf("*****************************\n");
 
-    // Radix Tests
+ //   // Radix Tests
 
-	int k = 4;
-	genArray(SIZE - 1, a, 1 << k); 
-    printArray(SIZE, a, true);
+	//int k = 4;
+	//genArray(SIZE - 1, a, 1 << k); 
+ //   printArray(SIZE, a, true);
 
-    zeroArray(SIZE, b);
-    printDesc("cpu sort, power-of-two");
-    StreamCompaction::CPU::sort(SIZE, b, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
-    printArray(SIZE, b, true);
+ //   zeroArray(SIZE, b);
+ //   printDesc("cpu sort, power-of-two");
+ //   StreamCompaction::CPU::sort(SIZE, b, a);
+ //   printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+ //   printArray(SIZE, b, true);
 
-    zeroArray(SIZE, c);
-    printDesc("cpu sort, non-power-of-two");
-    StreamCompaction::CPU::sort(NPOT, c, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
-    printArray(NPOT, c, true);
+ //   zeroArray(SIZE, c);
+ //   printDesc("cpu sort, non-power-of-two");
+ //   StreamCompaction::CPU::sort(NPOT, c, a);
+ //   printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+ //   printArray(NPOT, c, true);
 
-    zeroArray(SIZE, d);
-    printDesc("radix sort, power-of-two");
-    StreamCompaction::Radix::sort(SIZE, k + 1, d, a);
-    printElapsedTime(StreamCompaction::Radix::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    printArray(SIZE, d, true);
-	printCmpResult(SIZE, b, d);
+ //   zeroArray(SIZE, d);
+ //   printDesc("radix sort, power-of-two");
+ //   StreamCompaction::Radix::sort(SIZE, k + 1, d, a);
+ //   printElapsedTime(StreamCompaction::Radix::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
+ //   printArray(SIZE, d, true);
+	//printCmpResult(SIZE, b, d);
 
-    zeroArray(SIZE, d);
-    printDesc("radix sort, non-power-of-two");
-    StreamCompaction::Radix::sort(NPOT, k + 1, d, a);
-    printElapsedTime(StreamCompaction::Radix::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    printArray(NPOT, d, true);
-    printCmpResult(NPOT, c, d);
+ //   zeroArray(SIZE, d);
+ //   printDesc("radix sort, non-power-of-two");
+ //   StreamCompaction::Radix::sort(NPOT, k + 1, d, a);
+ //   printElapsedTime(StreamCompaction::Radix::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
+ //   printArray(NPOT, d, true);
+ //   printCmpResult(NPOT, c, d);
 
     system("pause"); // stop Win32 console from closing on exit
 }
