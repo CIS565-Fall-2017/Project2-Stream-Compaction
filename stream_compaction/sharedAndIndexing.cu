5ea96355446754309c6cc560d17d3d731f078953
Aman Sachan <aman9740@gmail.com>
2017-09-22 00:16:56 -0400

    wrote some shared memory scan stuff

--- stream_compaction/sharedAndIndexing.cu
+++ stream_compaction/sharedAndIndexing.cu
@@ -84,12 +84,80 @@ namespace StreamCompaction {
 			}
 		}
 
+		__global__ void kernScan_SharedMemory(int N, int* scan_out, int* sum)
+		{
+			extern __shared__ int shMemScanData[];
+			const int threadID_block = threadIdx.x;
+			const int threadID_grid = blockIdx.x * blockDim.x + threadIdx.x; 
+			int offset = 1;
+
+			// load input(which exists in dev_scan because work efficient scan happens in place) into shared memory
+			shMemScanData[2 * threadID_block] = scan_out[2 * threadID_block]; 
+			shMemScanData[2 * threadID_block + 1] = scan_out[2 * threadID_block + 1];
+
+			//UpSweep
+			for (int d = N >> 1; d > 0; d >>= 1)
+			{
+				__syncthreads();
+
+				if (threadID_block < d)
+				{
+					int leftChild = offset*(2 * threadID_block + 1) - 1;
+					int rightChild = offset*(2 * threadID_block + 2) - 1;
+
+					shMemScanData[rightChild] += shMemScanData[leftChild];
+				}
+				offset *= 2;
+			}
+			
+			if (gridDim.x > 1 && threadID_block == 0) ///CHECK TODO
+			{ 
+				sums[blockIdx.x] = shMemScanData[N-1];
+			}
+
+			// Clear the last element
+			if (threadID_block == 0) 
+			{
+				shMemScanData[N - 1] = 0;
+			}
+
+			//DownSweep
+			for (int d = 1; d < N; d <<= 1)
+			{
+				offset >>= 1;
+				__syncthreads();
+
+				if (threadID_block < d)
+				{
+					int leftChild = offset*(2 * threadID_block + 1) - 1;
+					int rightChild = offset*(2 * threadID_block + 2) - 1;
+
+					float inPlaceParent = shMemScanData[leftChild];
+					shMemScanData[leftChild] = shMemScanData[rightChild];
+					shMemScanData[rightChild] += inPlaceParent;
+				}
+			}
+
+			// Write Results to device memory
+			scan_out[2 * threadID_grid] = shMemScanData[2 * threadID_block];
+			scan_out[2 * threadID_grid + 1] = shMemScanData[2 * threadID_block + 1];
+		}
+
+		void scanImplementationWithSharedMemory(int log_n_ceil, int n, int pow2RoundedSize, int* dev_scan_out, int* dev_sum)
+		{
+			const int sharedMemorySize = pow2RoundedSize;
+			dim3 fullBlocksPerGrid_Strided((pow2RoundedSize + blockSize - 1) / blockSize);
+
+			kernScan_SharedMemory <<<fullBlocksPerGrid_Strided, blockSize, sharedMemorySize>>> (N, dev_scan_out, dev_sum);
+			checkCUDAError("DownSweep Failed!");
+		}
 		/**
 		* Performs prefix-sum (aka scan) on idata, storing the result into odata.
 		*/
 		void scan(int n, int *odata, const int *idata)
 		{
 			int* dev_scan_out;
+			int* dev_sum;
 
 			const int log_n_ceil = ilog2ceil(n);
 			const int pow2RoundedSize = 1 << log_n_ceil;
@@ -98,6 +166,9 @@ namespace StreamCompaction {
 
 			cudaMalloc((void**)&dev_scan_out, numbytes_pow2roundedsize);
 			checkCUDAErrorFn("cudaMalloc dev_scan_out failed!");
+			cudaMalloc((void**)&dev_sum, numbytes_pow2roundedsize);
+			checkCUDAErrorFn("cudaMalloc dev_scan_out failed!");
+			cudaMemset(dev_sum, 0, numbytes_pow2roundedsize);
 
 			cudaMemcpy(dev_scan_out, idata, numbytes_ForCopying, cudaMemcpyHostToDevice);
 			checkCUDAError("cudaMemcpy from idata to dev_scan_A failed!");
@@ -107,7 +178,9 @@ namespace StreamCompaction {
 			kernExcessZeroFill <<<fullBlocksPerGrid_Diff, blockSize >>> (pow2RoundedSize, n, dev_scan_out);
 
 			timer().startGpuTimer();
-				scanImplementation(log_n_ceil, n, pow2RoundedSize, dev_scan_out);
+				//scanImplementation(log_n_ceil, n, pow2RoundedSize, dev_scan_out);
+
+				scanImplementationWithSharedMemory(log_n_ceil, n, pow2RoundedSize, dev_scan_out, dev_sum);
 			timer().endGpuTimer();
 
 			cudaThreadSynchronize();
